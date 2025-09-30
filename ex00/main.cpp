ba084e1f900c5b974dba97425ab27f1e8bba4a86
Vladislav Sanin <vladislav.sanin.419@gmail.com>
2025-09-30 11:40:49 +0200

    validation mostly done. check negatives and overflows. todo: compare with data.csv and produce output

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsanin <vsanin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vsanin <vsanin@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:57:53 by vsanin            #+#    #+#             */
-/*   Updated: 2025/09/29 15:14:45 by vsanin           ###   ########.fr       */
+/*   Updated: 2025/09/30 11:39:26 by vsanin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <ctime>
 #include <cstdio>
 
-static bool validate_files(std::ifstream& input, std::ifstream& db)
+static bool validateFiles(std::ifstream& input, std::ifstream& db)
 {
 	if (!input.is_open())
 	{
@@ -40,28 +40,102 @@ void displayBTC(std::ifstream& input, std::map<std::string, float> &data)
 {
 	std::string line;
 	std::getline(input, line);
+    std::cout << line << std::endl;
 	if (line != "date | value")
 	{
 		std::cout << "Error: wrong input header formatting." << std::endl;
 		return;
 	}
-	int year, month, day;
+	int year, month, day, n;
+    float value;
 	while (std::getline(input, line))
 	{
-		/* 
-		1. find separator. if not found -> bad input
-		2. count length to separator. if not 11 -> bad input
-		3. substr from first 10/11 chars. it has to be the date.
-		4. verify the date, possibly with sscanf. simple char by char also possible.
-		5. keep date as string, will be checked against .csv.
-		6. check the input after separator - has to be int or float in range 0-1000. manual or stringstream.
-		7. produce the output.
-
+		// 5. keep date as string, will be checked against .csv.
+		// 6. check the input after separator - has to be int or float in range 0-1000. manual or stringstream.
+		// 7. produce the output.
+        
 		// todo?: similar checks for .csv just in case but debatable.
-		*/
-		std::string date = line.substr(0, 11);
-		int ret = std::sscanf(date.c_str(), "%d-%d-%d", &year, &month, &day);
-		
+
+        std::cout << line;
+        int size = line.size();
+        n = 0;
+		int fields = std::sscanf(line.c_str(), "%d-%d-%d | %f%n", &year, &month, &day, &value, &n); // %u for unsigned?
+        if (n < size && fields == 4)
+        {
+            std::cout << " ===> Error: bad input - extra character(s) found after value." << std::endl;
+            continue;
+        }
+        if (n == 0 || fields < 4)
+        {
+            std::cout << " ===> Error: bad input - filled " << fields << "/4 fields." << std::endl;
+            continue;
+        }
+        if (month < 1 || month > 12)
+        {
+            std::cout << " ===> Error: bad input - month out of range" << std::endl;
+            continue;
+        }
+        if (year < 2009)
+        {
+            std::cout << " ===> Error: bad input - bitcoin didn't exist back then." << std::endl;
+            continue;
+        }
+        if (year > 2025)
+        {
+            std::cout << " ===> Error: bad input - can't tell you future bitcoin value, sadly." << std::endl;
+            continue;
+        }
+        int failFlag = 0;
+        int leapYearFlag = 0;
+        int leapYears[] = {2012, 2016, 2020, 2024};
+        for (int i = 0; i < 4; i++)
+        {
+            if (year == leapYears[i])
+                leapYearFlag = 1;
+            if (leapYearFlag && month == 2 && (day < 1 || day > 29))
+            {
+                std::cout << " ===> Error: bad input - day out of range in February in a leap year" << std::endl;
+                failFlag = 1;
+                break;
+            }
+        }
+        if (failFlag)
+            continue;
+        if (!leapYearFlag && month == 2 && (day < 1 || day > 28))
+        {
+            std::cout << " ===> Error: bad input - day out of range in February in a regular year" << std::endl;
+            continue;
+        }
+        int longMonths[] = {1, 3, 5, 7, 8, 10, 12};
+        int shortMonths[] = {4, 6, 9, 11};
+        for (int i = 0; i < 7; i++)
+        {
+            if (month == longMonths[i] && (day < 1 || day > 31))
+            {
+                std::cout << " ===> Error: bad input - day out of range in a long month" << std::endl;
+                failFlag = 1;
+                break;
+            }
+        }
+        if (failFlag)
+            continue;
+        for (int i = 0; i < 4; i++)
+        {
+            if (month == shortMonths[i] && (day < 1 || day > 30))
+            {
+                std::cout << " ===> Error: bad input - day out of range in a short month" << std::endl;
+                failFlag = 1;
+                continue;
+            }
+        }
+        if (failFlag)
+            continue;
+        if (value < 0 || value > 1000)
+        {
+            std::cout << " ===> Error: bad input - value out of range" << std::endl;
+            continue;
+        }
+        std::cout << std::endl;
 	}
 	(void)data;
 }
@@ -75,7 +149,7 @@ int main(int argc, char** argv)
 	}
 	std::ifstream input(argv[1]);
 	std::ifstream db("data.csv");
-	if (!validate_files(input, db))
+	if (!validateFiles(input, db))
 		return 1;
 	BitcoinExchange exchange(db);
 	displayBTC(input, exchange.getData());
