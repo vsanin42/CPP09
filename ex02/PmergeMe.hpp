6664c1eaf99a242d133ad7428a365f9e0d4481b0
Vladislav Sanin <vladislav.sanin.419@gmail.com>
2026-01-21 16:21:16 +0100

    improved binary search, fixed pendsize < J-Jprev, hitting benchmarks. refactoring

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: vsanin <vsanin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:56:42 by vsanin            #+#    #+#             */
-/*   Updated: 2026/01/20 19:43:05 by vsanin           ###   ########.fr       */
+/*   Updated: 2026/01/21 16:11:16 by vsanin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,12 +72,12 @@ class PmergeMe
 		size_t getComparisons(void);
 		
 		template <typename T>
-		void initMainPend(T& cont, T& main, T& pend, size_t compFactor, size_t pairNum);
+		void initMainPend(T& cont, T& main, T& pend, size_t compFactor, size_t pairNum, size_t level);
 
 		template <typename T, typename U>
 		void structureMain(U& mainElements, T& main, size_t compFactor);
 		template <typename T, typename U>
-		void structurePend(U& pendElements, T& pend, size_t compFactor);
+		void structurePend(U& pendElements, T& pend, size_t compFactor, T& remainder, T& cont);
 		
 		void printContainers(PrintWhenOptions whenOption, PrintWhichOptions whichOption);
 		
@@ -88,6 +88,9 @@ class PmergeMe
 		
 		template <typename U>
 		size_t binarySearch(U& mainElements, U& pendElements, size_t startIndex);
+
+		template <typename U>
+		size_t findStartIndex(size_t J, U& pendElements, size_t startIndex, size_t insertionsCount);
 		
 		void FJMIEntry(std::vector<int>& cont);
 		void FJMIEntry(std::deque<int>& cont);
@@ -147,12 +150,7 @@ T PmergeMe::sortPairs(T& cont, size_t compFactor, size_t pairNum)
 	if (isOdd)
 	{
 		T remainder(cont.begin() + (compFactor * (2 * pairNum)), cont.end());
-		if (DEBUG)
-		{
-			std::cout << MAGENTA;
-			printContainer(remainder, "Remainder", 1, 1);
-			std::cout << RESET;
-		}
+		if (DEBUG) printContainer(remainder, "Remainder", 1, 1);
 		return remainder;
 	}
 	
@@ -164,7 +162,7 @@ T PmergeMe::sortPairs(T& cont, size_t compFactor, size_t pairNum)
 /*============================================================================*/
 
 template <typename T>
-void PmergeMe::initMainPend(T& cont, T& main, T& pend, size_t compFactor, size_t pairNum)
+void PmergeMe::initMainPend(T& cont, T& main, T& pend, size_t compFactor, size_t pairNum, size_t level)
 {
 	if (pairNum > 1)
 	{
@@ -177,6 +175,14 @@ void PmergeMe::initMainPend(T& cont, T& main, T& pend, size_t compFactor, size_t
 				i++;
 			}
 		}
+	}
+	if (DEBUG)
+	{
+		std::cout << BOLD YELLOW << "\nLevel: " << BCYAN << level << RESET << BOLD YELLOW << ", compFactor: " << BCYAN << compFactor << RESET << BOLD YELLOW << ", pairNum: " << BCYAN << pairNum << "\n" << RESET;
+		printContainer(cont, "Container", 2, 1);
+		printContainer(main, "Raw Main (b1 & a's)", 1, 1);
+		printContainer(pend, "Raw Pend (b's from b2)", 1, 1);
+		std::cout << "\n";
 	}
 }
 
@@ -191,11 +197,24 @@ void PmergeMe::structureMain(U& mainElements, T& main, size_t compFactor)
 }
 
 template <typename T, typename U>
-void PmergeMe::structurePend(U& pendElements, T& pend, size_t compFactor)
+void PmergeMe::structurePend(U& pendElements, T& pend, size_t compFactor, T& remainder, T& cont)
 {
 	size_t k = 2;
-	for (size_t i = 0; i < pend.size(); i += compFactor)
-		pendElements.push_back(std::make_pair(T(pend.begin() + i, pend.begin() + i + compFactor), k++));
+	if (cont.size() == 3 && remainder.size() == 1)
+	{
+		if (DEBUG)
+		{
+			std::cout << "\n3 number special case - couldn't form pend. Must insert remainder from sorting pairs: " << MAGENTA;
+			printContainer(remainder, "", 0, 1);
+			std::cout << RESET << "Creating pend with 1 remainder element\n";
+		}
+		pendElements.push_back(std::make_pair(T(1, remainder[0]), k));
+	}
+	else
+	{
+		for (size_t i = 0; i < pend.size(); i += compFactor)
+			pendElements.push_back(std::make_pair(T(pend.begin() + i, pend.begin() + i + compFactor), k++));
+	}
 	if (DEBUG) printPend(pendElements);
 }
 
@@ -232,45 +251,38 @@ void PmergeMe::printPend(U& pendElements)
 template <typename U>
 size_t PmergeMe::binarySearch(U& mainElements, U& pendElements, size_t startIndex)
 {
-	size_t rightmost = mainElements[0].first.size() - 1; // compFactor - 1
-	size_t upperBound = mainElements.size() - 1; // bad output below
+	size_t rightmost = mainElements[0].first.size() - 1;
+	size_t upperBound = mainElements.size();
 	if (DEBUG)
 	{
-		std::cout << BOLD BMAGENTA << "Binary search" << RESET << "\n";
-		std::cout << MAGENTA << "Defining search range" << RESET << "\n";
+		std::cout << BOLD BMAGENTA << "Binary search" << RESET << "\n" << MAGENTA << "Defining search range" << RESET << "\n";
 		printContainer(pendElements[startIndex].first, "Trying to insert element", 0, 1);
-		std::cout << "Currently searching main up to max element: " << BGREEN << "[" << mainElements[upperBound].second << "]\n" << RESET;
+		std::cout << "Currently searching main up to max element: " << BGREEN << "[" << (upperBound ? mainElements[upperBound - 1].second : mainElements[0].second) << "]\n" << RESET;
 	}
 	for (size_t i = 0; i < mainElements.size(); i++)
 	{
 		if (mainElements[i].second == pendElements[startIndex].second)
 		{
 			if (DEBUG) std::cout << "Found a corresponding index in main. The pend element being inserted will be smaller that that, search field will shrink\n";
-			i == 0 ? upperBound = 0 : upperBound = i - 1;
+			i == 0 ? upperBound = 0 : upperBound = i;
 			break;
 		}
 	}
 	
-	if (DEBUG)
-	{
-		std::cout << "After adjustment - Searching up to element: " << BGREEN << "[" << mainElements[upperBound].second << "]\n" << RESET;
-		std::cout << MAGENTA << "\nFinding an index for insertion\n" << RESET;
-	}
+	if (DEBUG) std::cout << "After adjustment - Searching up to element: " << BGREEN << "[" << (upperBound ? mainElements[upperBound - 1].second : mainElements[0].second) << "]\n" << MAGENTA << "\nFinding an index for insertion\n" << RESET;
 	size_t lowerBound = 0;
 	size_t mid = 0;
 	
-	// todo optimization
-	while (lowerBound != upperBound)
+	while (lowerBound < upperBound)
 	{
 		mid = lowerBound + ((upperBound - lowerBound) / 2);
 
 		if (DEBUG)
 		{
-			std::cout << "In range: " << ORANGE << lowerBound << RESET " - " << ORANGE << upperBound << RESET << " \t|\t";
-			std::cout << "Step 1: finding mid index: " ORANGE << mid << RESET << "\t|\t";
-			std::cout << "Step 2: comparing numbers: " << BCYAN << pendElements[startIndex].first[rightmost] << RESET BOLD << " ? " << RESET BCYAN << mainElements[mid].first[rightmost] << RESET << "\n";
+			std::cout << "In range: " << ORANGE << lowerBound << RESET " - " << ORANGE << upperBound << RESET << " \t|\tStep 1: finding mid index: " ORANGE << mid << RESET << "\t|\t";
+			std::cout << "Step 2: comparing numbers: " << BCYAN << pendElements[startIndex].first[rightmost] << RESET BOLD << " ? " << RESET BCYAN << mainElements[mid].first[rightmost] << RESET << "\t|\t";
 		}
-		if (pendElements[startIndex].first[rightmost] >= mainElements[mid].first[rightmost])
+		if (pendElements[startIndex].first[rightmost] > mainElements[mid].first[rightmost])
 		{
 			if (DEBUG) std::cout << "Updated " << BOLD << "lowerBound" << RESET << " - shifted up\n";
 			lowerBound = mid + 1;
@@ -278,32 +290,37 @@ size_t PmergeMe::binarySearch(U& mainElements, U& pendElements, size_t startInde
 		else
 		{
 			if (DEBUG) std::cout << "Updated " << BOLD << "upperBound" << RESET << " - shifted down\n";
-			upperBound = mid ? mid - 1 : 0;
+			upperBound = mid;
 		}
-		if (upperBound < lowerBound)
-			upperBound = lowerBound;
-		if (lowerBound > upperBound)
-			lowerBound = upperBound;
-		comparisons++;
-	}
-	if (DEBUG)
-	{
-		std::cout << BOLD << "lowerBound == upperBound" << RESET << " - potential index for insertion found: " << ORANGE << lowerBound << RESET << "\n";
-		std::cout << "Comparing number to insert (" << BCYAN << pendElements[startIndex].first[rightmost] << RESET
-				  << ") to current number at the index (" << BCYAN << mainElements[lowerBound].first[rightmost] << RESET << ")\n";
-	}
-	if (pendElements[startIndex].first[rightmost] > mainElements[lowerBound].first[rightmost])
-	{
-		if (DEBUG) std::cout << "Updating index (++) / swapping elements\n";
-		lowerBound++;
-		comparisons++;
-	}
-	else
-	{
-		if (DEBUG) std::cout << "Index unchanged after final comparison\n";
 		comparisons++;
 	}
 	return lowerBound;
+}
+
+template <typename U>
+size_t PmergeMe::findStartIndex(size_t J, U& pendElements, size_t startIndex, size_t insertionsCount)
+{
+	if (DEBUG) std::cout << BBLUE << "\nCurrent selected Jacobstahl number: " << BOLD << J << RESET << "\n";
+	size_t pendSize = pendElements.size();
+	if (pendSize == 0)
+		return 0;
+	if (insertionsCount > 0)
+		return startIndex ? startIndex - 1 : 0;
+	if (J == 0)
+	{
+		if (DEBUG) std::cout << YELLOW "Ran out of Jacobstahl numbers. Inserting the remaining pend elements in reverse order\n" << RESET;
+		return pendSize - 1;
+	}
+	for (size_t i = 0; i < pendSize; i++)
+	{
+		if (pendElements[i].second == J)
+		{
+			if (DEBUG) std::cout << "Starting inserting with pend element " << BGREEN << "[" << pendElements[i].second << "]" << RESET << ", index matches " << BBLUE "J\n" << RESET;
+			return i;
+		}
+	}
+	if (DEBUG) std::cout << "Pend index matching " << BBLUE << "J" << RESET << " not found. Starting inserting in reverse from element " BGREEN << "[" << pendElements[pendSize - 1].second << "]" << RESET << "\n";
+	return pendSize - 1;
 }
 
 /*============================================================================*/
@@ -355,12 +372,7 @@ void PmergeMe::FJMICore(T& cont, U& mainElements, size_t level)
 	
 	if (DEBUG) printContainer(cont, "Container before pair sorting", 1, 1);
 	T remainder = sortPairs(cont, compFactor, pairNum);
-	if (DEBUG)
-	{
-		std::cout << GREEN;
-		printContainer(cont, "Container after pair sorting", 1, 1);
-		std::cout << RESET;
-	}
+	if (DEBUG) printContainer(cont, "Container after pair sorting", 1, 1);
 	
 	// 3. Recursion logic - all sorting in the previous step must happen first, then insertion on unwind.
 	FJMICore(cont, mainElements, level + 1);
@@ -370,18 +382,7 @@ void PmergeMe::FJMICore(T& cont, U& mainElements, size_t level)
 	T main(cont.begin(), cont.begin() + (compFactor * 2));
 	T pend;
 
-	initMainPend(cont, main, pend, compFactor, pairNum);
-
-	if (DEBUG)
-	{
-		std::cout << BOLD YELLOW << "\nLevel: " << BCYAN << level << RESET
-				  << BOLD YELLOW << ", compFactor: " << BCYAN << compFactor << RESET
-				  << BOLD YELLOW << ", pairNum: " << BCYAN << pairNum << "\n" << RESET;
-		printContainer(cont, "Container", 2, 1);
-		printContainer(main, "Raw Main (b1 & a's)", 1, 1);
-		printContainer(pend, "Raw Pend (b's from b2)", 1, 1);
-		std::cout << "\n";
-	}
+	initMainPend(cont, main, pend, compFactor, pairNum, level);
 
 	// 4.2. Create a Jacobstahl sequence for the current level and pend size.
 	size_t pendElementsCount = pend.size() / compFactor;
@@ -403,70 +404,42 @@ void PmergeMe::FJMICore(T& cont, U& mainElements, size_t level)
 	
 	U pendElements;
 
-	if (cont.size() == 3 && remainder.size() == 1)
-	{
-		if (DEBUG)
-		{
-			std::cout << "\n3 number special case - couldn't form pend. Must insert remainder from sorting pairs: " << MAGENTA;
-			printContainer(remainder, "", 0, 1);
-			std::cout << RESET << "Creating pend with 1 remainder element\n";
-		}
-		pendElements.push_back(std::make_pair(T(1, remainder[0]), 2));
-		if (DEBUG) printPend(pendElements);
-	}
-	else
-		structurePend(pendElements, pend, compFactor);
+	structurePend(pendElements, pend, compFactor, remainder, cont);
 
 	if (DEBUG) std::cout << "\n---\n";
 
-	int n = 0;
-	size_t insertionsCount = 0;
-	size_t startIndex = 0;
-	while (pendElements.size())
+	// 4.4. Insert elements from pend into main based on Jacobstahl numbers ordering.
+	size_t n = 0, insertionsCount = 0, startIndex = 0;
+	while (!pendElements.empty())
 	{
-		if ((n > 0 && insertionsCount == jacobstahlSequence[n] - jacobstahlSequence[n - 1]) || (n == 0 && insertionsCount == 2))
+		size_t J = (n >= jacobstahlSequence.size()) ? 0 : jacobstahlSequence[n];
+		
+		if (J != 0)
 		{
-			n++;
-			insertionsCount = 0;
-		}
-			
-		size_t J = 0;
-		if (n >= static_cast<int>(jacobstahlSequence.size()) || n == -1)
-		{
-			n = -1;
-			J = 0;
-		}
-		else
-			J = jacobstahlSequence[n];
-
-		if (DEBUG) std::cout << BBLUE << "\nCurrent selected Jacobstahl number: " << BOLD << J << RESET << "\n";
-		if (J > 0)
-		{
-			for (size_t i = 0; i < pendElements.size(); i++)
+			size_t blockSize = (n == 0) ? 2 : (jacobstahlSequence[n] - jacobstahlSequence[n - 1]);
+			if (insertionsCount == blockSize)
 			{
-				if (pendElements[i].second == J)
-				{
-					startIndex = i;
-					if (DEBUG) std::cout << "Starting inserting with pend element " << BGREEN << "[" << pendElements[i].second << "]" << RESET << ", index matches " << BBLUE "J\n" << RESET;
-				}
+				n++;
+				insertionsCount = 0;
+				continue;
 			}
 		}
-		else
-		{
-			if (DEBUG) std::cout << YELLOW "Ran out of Jacobstahl numbers. Inserting the remaining pend elements in reverse order\n" << RESET;
-			startIndex = pendElements.size() - 1;
-		}
+
+		startIndex = findStartIndex(J, pendElements, startIndex, insertionsCount);
 		if (DEBUG) std::cout << "Start index: " << startIndex << "\n";
 		size_t insertionIndex = binarySearch(mainElements, pendElements, startIndex);
 		mainElements.insert(mainElements.begin() + insertionIndex, pendElements[startIndex]);
 		pendElements.erase(pendElements.begin() + startIndex);
-		if (DEBUG) printMain(mainElements);
-		if (DEBUG) printPend(pendElements);
-		startIndex > 0 ? startIndex-- : startIndex = 0;
+		if (DEBUG)
+		{
+			printMain(mainElements);
+			printPend(pendElements);
+			std::cout << "\n---\n";
+		}
 		insertionsCount++;
-		if (DEBUG) std::cout << "\n---\n";
 	}
 
+	// 4.5. Fill the original container with the result of insertion at this level to carry over to the next level.
 	if (DEBUG) printContainer(cont, "\nOriginal container before insertion at this level", 1, 1);
 	for (size_t el = 0; el < mainElements.size(); el++)
 	{
